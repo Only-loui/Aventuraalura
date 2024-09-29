b7141edf80d6663b7df2ddfde51807e1c10694af
Maria Luiza Soares Pereira <maridesenhos15@gmail.com>
2024-09-29 17:35:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Maria Luiza Soares Pereira 3D 23/09/24
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
